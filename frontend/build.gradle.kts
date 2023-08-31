1944d2874bba96775c7b00b5f78538ccad3544dc
kigawa <kigawa.8390@gmail.com>
2023-08-31 15:31:49 +0900

    fix: gradle

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,36 +1,40 @@
 plugins {
-    kotlin("js")
+  kotlin("js")
 }
 
 group = "net.kigawa"
 version = "0.0.1"
 val reactVersion = project.property("react.version") as String
 val reactRouterVersion = project.property("react-router.version") as String
+val emotionVersion = project.property("emotion.version") as String
 val kotlinWrappersPrefix = project.property("kotlin.wrappers.prefix") as String
-val kotlinWrapperReactVersion = reactVersion + kotlinWrappersPrefix
-val kotlinWrapperReactRouterVersion = reactRouterVersion + kotlinWrappersPrefix
+
+val kotlinWrapperReactVersion = "$reactVersion$kotlinWrappersPrefix"
+val kotlinWrapperReactRouterVersion = "$reactRouterVersion$kotlinWrappersPrefix"
+val kotlinWrapperEmotionVersion = "$emotionVersion$kotlinWrappersPrefix"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$kotlinWrapperReactVersion")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$kotlinWrapperReactVersion")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:$kotlinWrapperReactRouterVersion")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$kotlinWrapperReactVersion")
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$kotlinWrapperReactVersion")
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:$kotlinWrapperReactRouterVersion")
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:$kotlinWrapperEmotionVersion")
 
-    implementation(npm("postcss", "8.2.6"))
-    implementation(npm("postcss-loader", "4.2.0"))
-    implementation(npm("tailwindcss", "2.0.3"))
+  implementation(npm("postcss", "8.2.6"))
+  implementation(npm("postcss-loader", "4.2.0"))
 }
 
 kotlin {
-    js(IR) {
-        browser {
-            runTask {
-                devServer?.apply {
-                    port = 8081
-                    proxy = mutableMapOf(Pair("/api", "http://127.0.0.1:8080/api"))
-                }
-            }
+  js(IR) {
+    browser {
+      runTask {
+        devServer?.apply {
+          port = 8081
+          proxy = mutableMapOf(Pair("/api", "http://127.0.0.1:8080"))
+          static !!.add("./resources/public")
         }
-        binaries.executable()
+      }
     }
+    binaries.executable()
+  }
 }
